f6aff6452e1b5c464679878823c8df3077684dba
Ivo de Kler <ivo@q42.nl>
2012-05-14 17:07:25 +0200

    First commit

---
+++ colorObject.js
@@ -0,0 +1,47 @@
+function Color(options){
+	this.options = $.extend({
+		r: 0,
+		g: 0,
+		b: 0,
+		a: 1
+	}, options || {});
+}
+
+Color.prototype.randomize = function(){
+	this.options.r = Math.floor(Math.random() * 256);
+	this.options.g = Math.floor(Math.random() * 256);
+	this.options.b = Math.floor(Math.random() * 256);
+	return this;
+}
+
+Color.prototype.toRGBString = function(){
+	return 'rgb(' + [this.options.r, this.options.g, this.options.b].join(',') + ')';
+}
+
+Color.prototype.toRGBAString = function(){
+	return 'rgba(' + [this.options.r, this.options.g, this.options.b, this.options.a].join(',') + ')';
+}
+
+Color.prototype.toArray = function(){ return [this.options.r, this.options.g, this.options.b, this.options.a ]}
+
+Color.prototype.averageWith = function(colors){
+	if(colors instanceof Color) colors = [colors];
+	var r = this.options.r, g = this.options.g, b = this.options.b, a = this.options.a, length = colors.length + 1, index;
+	for(index in colors){
+		r += colors[index].options.r;
+		g += colors[index].options.g;
+		b += colors[index].options.b;
+		a += colors[index].options.a;
+	}
+	return new Color({r: Math.floor(r/length), g: Math.floor(g/length), b: Math.floor(b/length), a: Math.floor(a/length) });
+}
+
+function compareArrays(arr1, arr2, power){
+	if(!power) power = 1;
+	var returnVal = 0, i = 0, len = arr1.length;
+	while(i < len){
+		returnVal += Math.pow(Math.max(arr1[i], arr2[i]) - Math.min(arr1[i], arr2[i]), power);
+		i++;
+	}
+	return returnVal;
+}
---
+++ tasktree.js
@@ -0,0 +1,101 @@
+var $canvas = $('canvas'),
+		ctx = $canvas[0].getContext('2d'),
+		sys = arbor.ParticleSystem({
+			precision: 0,
+			replusion: 2000
+		}),
+		width, height,
+		bgnd = new Color({r: 255, g: 255, b: 255, a: 0.05}),
+		bgndString = bgnd.toRGBAString();
+
+function resize(){
+	width = $canvas.parent().width();
+	height = $canvas.parent().height();
+	$canvas.attr('width', width).attr('height', height);
+}
+
+$(window).on('resize', resize);
+resize();
+
+var myRenderer = {
+	init: function(){
+		ctx.strokeStyle = '#000000';
+		ctx.fillStyle = '#FF0000';
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		ctx.arc(100,100, 100, 0, 2*Math.PI, true);
+		ctx.closePath();
+		ctx.stroke();
+		console.log('reached end of init');
+	},
+	redraw: function(){
+		ctx.save();
+		//ctx.clearRect(0,0, width, height);
+		ctx.fillStyle = bgndString;
+		ctx.fillRect(0,0,width,height);
+		
+    sys.eachEdge(function(edge, pt1, pt2){
+			ctx.beginPath();
+			ctx.moveTo(pt1.x, pt1.y);
+			ctx.lineTo(pt2.x, pt2.y);
+			
+			ctx.strokeStyle = edge.data.color.toRGBString();
+			ctx.stroke();
+		});
+		
+		sys.eachNode(function(node, pt){
+			ctx.beginPath();
+			ctx.arc(pt.x, pt.y, Math.sqrt(node.mass), 0, 2*Math.PI, true);
+			ctx.closePath();
+			
+			ctx.fillStyle = node.data.color.toRGBString();
+			ctx.fill();
+		});
+		ctx.restore()
+	}
+}
+
+sys.renderer = myRenderer;
+
+var nodes = [], edges = [];
+
+$(document).on('click', function(e){
+	addConnectedNode(e.clientX, e.clientY);
+});
+
+function addConnectedNode(x, y){
+	nodes.push(
+		sys.addNode(
+			'p' + new Date().getTime(),
+			{
+				x: x,
+				y: y,
+				mass: (Math.random() * 100) + 10,
+				color: new Color().randomize()
+			}
+		)
+	);
+	var i = 0, len = nodes.length - 1,
+			toNode = nodes[len], fromNode, length, color;
+	while(i < len){
+		fromNode = nodes[i];
+		length = compareArrays(fromNode.data.color.toArray(), toNode.data.color.toArray(), 1);
+		color = fromNode.data.color.averageWith(toNode.data.color);
+		edges.push( sys.addEdge(fromNode, toNode, { length: length , color: color }) );
+		i++;
+	}
+}
+
+function networkN(n){
+	var x = Math.floor(width/2), y = Math.floor(height/2);
+	addConnectedNode(x, y);
+	setTimeout(function(){
+		var i = 0;
+		while(i < n){
+			setTimeout(function(){ addConnectedNode(x, y); }, i);
+			i++;
+		}
+	}, 100);
+}
+
+$(document).on('ready', function(){ networkN(50); })
