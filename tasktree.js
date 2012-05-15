aad721fb5b8cc33d823db80917a3e8f15d26f08a
Ivo de Kler <ivo@q42.nl>
2012-05-15 17:42:43 +0200

    Line width varies with compression ratio

--- tasktree.js
+++ tasktree.js
@@ -1,10 +1,10 @@
 var $canvas = $('canvas'),
 		ctx = $canvas[0].getContext('2d'),
 		sys = arbor.ParticleSystem({
-			precision: 5,
-			repulsion: 1,
-			stiffness: 100,
-			friction: 0.2
+			precision: 0,
+			repulsion: 10000000,
+			stiffness: 0,
+			friction: 0
 		}),
 		width, height,
 		bgnd = new Color({r: 0, g: 0, b: 0, a: 0.01}),
@@ -24,6 +24,7 @@ resize();
 var myRenderer = {
 	init: function(){
 		ctx.lineWidth = 1;
+		ctx.lineCap = 'round';
 		ctx.fillStyle = new Color({r: 255, g: 255, b: 255}).toRGBString();
 		ctx.fillRect(0,0,width,height);
 	},
@@ -38,6 +39,11 @@ var myRenderer = {
 			ctx.moveTo(pt1.x, pt1.y);
 			ctx.lineTo(pt2.x, pt2.y);
 			
+			var actualLength = Math.sqrt( Math.pow( Math.abs(pt1.x - pt1.x), 2 ) + Math.pow( Math.abs(pt1.y - pt2.y), 2) );
+			
+			edge.data.ratio = Math.max(edge.length, actualLength) / Math.min(edge.length, actualLength);
+			
+			ctx.lineWidth = Math.max(1, Math.pow(edge.data.ratio, .3) * 2);
 			ctx.strokeStyle = edge.data.color.toRGBString();
 			ctx.stroke();
 		});
@@ -102,13 +108,13 @@ function networkN(n){
 }
 
 $(document).on('ready', function(){
-	networkN(Math.ceil(Math.random()*25));
+	networkN(Math.ceil(Math.random()*0));
 	var amt = 0;
 	setInterval(function(){
 		amt++;
 		var fps = sys.fps();
 		$('#fps').html(fps);
-		if(amt%2 && fps > 8) addConnectedNode(Math.floor(width/2),Math.floor(height/2));
+		if(amt%2 && fps > 4) addConnectedNode(Math.floor(width/2),Math.floor(height/2));
 	}, 1000);
 	setTimeout(function(){
 		if($('#fps').html() == 'Infinity') location.reload();
