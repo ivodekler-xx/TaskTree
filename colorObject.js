bcf2d5f89fbbea6cf09e4b0f0037b2ad31c230df
Ivo de Kler <ivo@q42.nl>
2012-05-16 15:18:26 +0200

    Improved color object

--- colorObject.js
+++ colorObject.js
@@ -1,41 +1,41 @@
 function Color(options){
-	this.options = $.extend({
+	$.extend(this, $.extend({
 		r: 0,
 		g: 0,
 		b: 0,
 		a: 1
-	}, options || {});
+	}, options || {}));
 }
 
 Color.prototype.randomize = function(){
-	this.options.r = Math.floor(Math.random() * 256);
-	this.options.g = Math.floor(Math.random() * 256);
-	this.options.b = Math.floor(Math.random() * 256);
+	this.r = Math.floor(Math.random() * 256);
+	this.g = Math.floor(Math.random() * 256);
+	this.b = Math.floor(Math.random() * 256);
 	return this;
 }
 
 Color.prototype.invert = function(){
-	return new Color({r: 255 - this.options.r, g: 255 - this.options.g, b: 255 - this.options.b });
+	return new Color({r: 255 - this.r, g: 255 - this.g, b: 255 - this.b });
 }
 
 Color.prototype.toRGBString = function(){
-	return 'rgb(' + [this.options.r, this.options.g, this.options.b].join(',') + ')';
+	return 'rgb(' + [this.r, this.g, this.b].join(',') + ')';
 }
 
 Color.prototype.toRGBAString = function(){
-	return 'rgba(' + [this.options.r, this.options.g, this.options.b, this.options.a].join(',') + ')';
+	return 'rgba(' + [this.r, this.g, this.b, this.a].join(',') + ')';
 }
 
-Color.prototype.toArray = function(){ return [this.options.r, this.options.g, this.options.b, this.options.a ]}
+Color.prototype.toArray = function(){ return [this.r, this.g, this.b, this.a ]}
 
 Color.prototype.averageWith = function(colors){
 	if(colors instanceof Color) colors = [colors];
-	var r = this.options.r, g = this.options.g, b = this.options.b, a = this.options.a, length = colors.length + 1, index;
+	var r = this.r, g = this.g, b = this.b, a = this.a, length = colors.length + 1, index;
 	for(index in colors){
-		r += colors[index].options.r;
-		g += colors[index].options.g;
-		b += colors[index].options.b;
-		a += colors[index].options.a;
+		r += colors[index].r;
+		g += colors[index].g;
+		b += colors[index].b;
+		a += colors[index].a;
 	}
 	return new Color({r: Math.floor(r/length), g: Math.floor(g/length), b: Math.floor(b/length), a: Math.floor(a/length) });
 }
